29e835b0ff5351c27bece06555ce3cf36c902460
kluory <kluory@gmail.com>
2025-08-18 21:41:17 +0300

    warrior rotation begin

--- Data/Warrior.lua
+++ Data/Warrior.lua
@@ -42,10 +42,29 @@ local function getSpec3Action()
         return 'none', 'кастую [' .. ns.State.playerCasting .. ']'
     end
     -- тут что-то делаем бафы, хилки, и т.д. (Цели тут может и не быть)
+    local rage = UnitMana('player')
+    local stance = GetShapeshiftForm()
+
+    if stance ~= 2 and ns.State.combatLock and ns.CanUseAction('Оборонительная стойка') then
+         return 'Оборонительная стойка', 'свитч в дэфстэнс в бою'
+    end
+  
     local tarcmd, tarinfo = ns.TryTarget()
     if tarcmd then
         return tarcmd, tarinfo
     end
+
+    local inMelee = ns.IsSpellInRange('Кровопускание')
+
+    -- Инициация боя: Рывок
+    if ns.State.attack and ns.CanUseAction('Рывок') and ns.TimerMore('Перехват', 1.5) then
+        return 'Рывок', 'Рывок'
+    end
+    -- Или Перехват
+    if ns.State.attack and ns.CanUseAction('Перехват') and ns.TimerMore('Рывок', 1.5) then
+        return 'Перехват', 'Перехват'
+    end
+
     -- тут ротацию ишем, можно испольовать что можно прожвать в гкд
     if ns.State.gcd then return 'none', 'гкд' end
     -- то что требуется гкд
