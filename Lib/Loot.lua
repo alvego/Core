380efb878bfa5ffdb1362e0e2c7ef780f5135e5e
alvego <alvego@gmail.com>
2025-11-10 10:24:34 +0300

    untested loot

--- Lib/Loot.lua
+++ Lib/Loot.lua
@@ -28,16 +28,21 @@ local canLoot = c.GetCachedFunc(function(unit)
     return c.ReadByte(ptr, 168) ~= 0
 end)
 
+local lootList = {}
+local lootTimer = 'lootTimer'
+local lootDist = 5
+
 local function checkCorpseForLoot(unit)
     if not UnitExists(unit) then return end
     if not UnitIsDead(unit) then return end
     if UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then return end
     if UnitIsPlayer(unit) then return end
-    if c.UnitDistance('player', unit) > 5 then return end -- so far
-    if not canLoot(unit) then return end                  -- can't loot
+    if c.UnitDistance('player', unit) > lootDist then return end -- so far
+    if not canLoot(unit) then return end                         -- can't loot
+    if tContains(lootList, unit) then return end
     return unit
 end
-
+-------------------------------------------------------------------------------
 local canSkin = c.GetCachedFunc(function(unit)
     local creatureType = UnitCreatureType(unit)
     return creatureType == 'Животное' and not canLoot(unit)
@@ -45,30 +50,34 @@ end)
 
 local skinList = {}
 local skinTimer = 'skinTimer';
+local skinDist = 8
 
 local function checkCorpseForSkin(unit)
     if not UnitExists(unit) then return end
     if not UnitIsDead(unit) then return end
     if UnitIsPlayer(unit) then return end
-    if c.UnitDistance('player', unit) > 8 then return end -- so far
-    if not canSkin(unit) then return end                  -- can't skin
+    if c.UnitDistance('player', unit) > skinDist then return end -- so far
+    if not canSkin(unit) then return end                         -- can't skin
     if tContains(skinList, unit) then return end
     return unit
 end
 
-
+-------------------------------------------------------------------------------
+local maxDist = 40
 local _corpse, _dist = nil, 0
 local function checkCorpse(unit, allowSkin)
     if not UnitExists(unit) then return end
     if not UnitExists(unit) then return end
     if not UnitIsDead(unit) then return end
     if UnitIsPlayer(unit) then return end
-    if UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit) then return end
-    if tContains(skinList, unit) then return end
-    if not (canLoot(unit) or (allowSkin and canSkin(unit))) then return end
+    local loot = not (
+        tContains(lootList, unit) or (UnitIsTapped(unit) and not UnitIsTappedByPlayer(unit))
+    ) and canLoot(unit)
+    local skin = allowSkin and not (loot or tContains(skinList, unit)) and canSkin(unit)
+    if not (loot or skin) then return end
     if not c.UnitInLOS(unit) then return end
     local dist = c.UnitDistance('player', unit)
-    if dist < 5 or dist > 40 then return end
+    if dist < (loot and lootDist or skinDist) or dist > maxDist then return end
     if not _corpse or not _dist or dist < _dist then
         _corpse = unit
         _dist = dist
@@ -81,82 +90,82 @@ local function getNearCorpse(allowSkin)
     return _corpse
 end
 -------------------------------------------------------------------------------
-local bobberUID = nil
-local isBobbingTimer = 'isBobbing'
-local fishSpell = 'Рыбная ловля'
+local fish = {}
+fish.spell = 'Рыбная ловля'
+fish.bobber = nil
+fish.delay = 0
+
 c.AttachEvent('COMBAT_LOG_EVENT_UNFILTERED',
     function(event, timestamp, subEvent, sourceGUID, sourceName, sourceFlags, destGUID, destName,
              destFlags, ...)
         local spellName = select(2, ...)
-        if subEvent:match('SPELL_CREATE') and sourceGUID == c.state.playerGUID and spellName == fishSpell then
-            bobberUID = c.GetObjectIdByGUID(destGUID)
-            c.TimerReset(isBobbingTimer)
+        if subEvent:match('SPELL_CREATE') and sourceGUID == st.playerGUID and spellName == fish.spell then
+            fish.bobber = c.GetObjectIdByGUID(destGUID)
             c.Log('Видим поплавок')
-            c.TimerStart(fishSpell)
+            c.TimerStart(fish.spell)
+            fish.delay = 0.5 + math_random() * 2.5 -- [0 .. 3]
         end
     end
 )
 -------------------------------------------------------------------------------
 
+local function lootUnit(unit)
+    c.Message(c.UnitInfo(unit), 'Лутаем')
+    c.UnitClick(unit, true)
+    c.TimerStart(lootTimer)
+    tinsert(lootList, unit)
+end
+
+local function waitForLoot()
+    local isOpenLoot = LootFrame:IsVisible()
+    c.TimerToggle('LootFrame', isOpenLoot) -- таймер идет пока открыт LootFrame
+    if isOpenLoot then
+        -- LootFrame висит уже 1.5 секунды
+        local IsLootLag = c.TimerStarted('needHeal') and c.TimerMore('needHeal', 1.5)
+        -- если окно лута подвисло
+        if IsLootLag then
+            for i = 1, GetNumLootItems() do
+                if not select(5, GetLootSlotInfo(i)) then LootSlot(i) end
+            end
+            CloseLoot()
+        end
+        return true -- ждем закрытия лута
+    end
+    return false    -- лут нет, можно что-то делать
+end
+
+-------------------------------------------------------------------------------
+
 c.AttachBeforeUpdate(function()
     if c.Paused() then return end
-    if c.TimerLess('Loot', 0.5) then return end
-    if c.TimerStarted(skinTimer) and c.TimerMore(skinTimer, 5) then wipe(skinList) end
-    if st.gcd or st.mounted or st.combatMode then return end
     if st.group and (GetLootMethod() ~= 'freeforall') then return end
     if c.GetBagsFreeSlots() < 1 then return end
     if GetCVar('autoLootDefault') ~= '1' then return end
-    -- если окно лута подвисло
-    if LootFrame:IsVisible() then
-        for i = 1, GetNumLootItems() do
-            if not select(5, GetLootSlotInfo(i)) then LootSlot(i) end
-        end
-        CloseLoot()
-        return
-    end
+    if waitForLoot() then return end
+    if c.TimerLess('Loot', 0.5) then return end
+    if c.TimerStarted(lootTimer) and c.TimerMore(lootTimer, 5) then wipe(lootList) end
+    if c.TimerStarted(skinTimer) and c.TimerMore(skinTimer, 5) then wipe(skinList) end
+    if st.gcd or st.mounted or st.combatMode then return end
 
-    if IsEquippedItemType('Удочка') and IsUsableSpell(fishSpell) and c.TimerLess(fishSpell, 5) then
+    if IsEquippedItemType('Удочка') and IsUsableSpell(fish.spell) and c.TimerLess(fish.spell, 5) then
         if not st.still then return end
 
         if not st.playerCasting then
-            local salt = 0.5 + math_random() * 2.5 -- [0 .. 3]
-            if c.CanUseGcdSpell(fishSpell, nil, salt) then
-                c.DoAction('Забрасываем', fishSpell)
-                c.TimerStart(fishSpell)
-            elseif c.TimerMore(fishSpell, 0.5) then
-                c.Log('Может еще закинуть?')
+            if c.CanUseGcdSpell(fish.spell, nil, fish.delay) then
+                c.DoAction('Забрасываем', fish.spell)
+                c.SkipNextUpdate()
+                c.TimerStart(fish.spell)
             end
-            c.SkipNextUpdate()
             return
         end
 
-        if st.playerCasting == fishSpell then
-            c.TimerStart(fishSpell)
-            if bobberUID and UnitName(bobberUID) then
-                if not c.TimerStarted(isBobbingTimer) then
-                    local ptr = c.UnitPtr(bobberUID)
-                    if c.ReadByte(ptr, 188) == 1 then
-                        c.Log('Клюнуло')
-                        c.TimerStart(isBobbingTimer)
-                    end
-                end
-
-                if c.TimerStarted(isBobbingTimer) then
-                    local delay = 0.5 + math_random() * 1.5 -- [0 .. 2]
-                    if c.TimerMore(isBobbingTimer, delay) then
-                        c.Log('Подсекаем')
-                        c.UnitClick(bobberUID, true)
-                        bobberUID = nil
-                        c.TimerReset(isBobbingTimer)
-                    else
-                        c.Log('Наверное надо тянуть?')
-                    end
-                end
-                c.SkipNextUpdate()
-                return
-            end
+        if st.playerCasting == fish.spell and fish.bobber then
+            c.TimerStart(fish.spell)
+            local ptr = c.UnitPtr(fish.bobber)
+            if c.ReadByte(ptr, 188) ~= 1 then return end
+            lootUnit(fish.bobber)
+            fish.bobber = nil
         end
-        c.SkipNextUpdate()
         return
     end
 
@@ -164,9 +173,7 @@ c.AttachBeforeUpdate(function()
     -- ищем кого можно лутануть
     local corpse = c.FindValue(c.GetUnits(), checkCorpseForLoot)
     if corpse then
-        c.Message(c.UnitInfo(corpse), 'Лутаем')
-        c.UnitClick(corpse, true)
-        c.SkipNextUpdate()
+        lootUnit(corpse)
         c.TimerStart('Loot')
         return
     end
@@ -182,7 +189,6 @@ c.AttachBeforeUpdate(function()
             c.DoAction('Свежуем', skinSpell, corpse)
             tinsert(skinList, corpse)
             c.TimerStart(skinTimer)
-            c.SkipNextUpdate()
             c.TimerStart('Loot', 1.5)
             return
         end
@@ -192,7 +198,7 @@ c.AttachBeforeUpdate(function()
     if st.look then return end
     -- ищем ближайший полезный труп
     corpse = getNearCorpse(allowSkin)
-    if corpse and c.PlayerMove(corpse, 40) then
+    if corpse and c.PlayerMove(corpse, maxDist) then
         c.TimerStart('Loot', 3)
     end
 end)
